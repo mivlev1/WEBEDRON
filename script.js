f293139205b54826791e4d65714707f86cb17b31
Alexander <poltavetz.alexander@mail.ru>
2022-01-13 03:06:52 +0300

    Form js

--- script.js
+++ script.js
@@ -76,3 +76,68 @@ $(document).ready(function() {
       }
     });
   });
+//Send form
+function saveLocalStorage() {
+    localStorage.setItem("footer-name", $("#footer-name").val());
+    localStorage.setItem("footer-number", $("#footer-number").val());
+    localStorage.setItem("footer-email", $("#footer-email").val());
+    localStorage.setItem("footer-message", $("#footer-message").val());
+    localStorage.setItem("footer-policy", $("#footer-policy").prop("checked"));
+}
+
+function loadLocalStorage() {
+    if (localStorage.getItem("name") !== null)
+        $("#footer-name").val(localStorage.getItem("footer-name"));
+    if (localStorage.getItem("number") !== null)
+        $("#footer-number").val(localStorage.getItem("footer-number"));
+    if (localStorage.getItem("email") !== null)
+        $("#footer-email").val(localStorage.getItem("footer-email"));
+    if (localStorage.getItem("message") !== null)
+        $("#footer-message").val(localStorage.getItem("footer-message"));
+    if (localStorage.getItem("policy") !== null) {
+        $("#footer-policy").prop("checked", localStorage.getItem("footer-policy") === "true");
+        if ($("#footer-policy").prop("checked"))
+            $("#sendButton").removeAttr("disabled");
+    }
+}
+function clear() {
+    localStorage.clear()
+    $("#footer-name").val("");
+    $("#footer-number").val("");
+    $("#footer-email").val("");
+    $("#footer-message").val("");
+    $("#footer-policy").val(false);
+}
+
+$(document).ready(function () {
+    loadLocalStorage();
+    $("#footer-form").submit(function (e) {
+        e.preventDefault();
+        let data = $(this).serialize();
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "https://formcarry.com/s/a4nsVGvnu53",
+            data: data,
+            success: function (response) {
+                if (response.status == "success") {
+                    alert("Форма отправлена");
+                    clear();
+                } else {
+                    alert("Произошла ошибка: " + response.message);
+                }
+            }
+        });
+    });
+
+    $("#footer-policy").change(function () {
+        if ((!this.checked))
+            $("#sendButton").attr("disabled", "");
+        else
+            $("#sendButton").removeAttr("disabled");
+    })
+
+
+    $("#footer-form").change(saveLocalStorage);
+})
